08158f85dc714b4fda70a46268ee1da3ce6fc088
Hyojin <oneNmore.jin@gmail.com>
2024-08-20 22:17:58 +0900

    Time: 50 ms (63.47%), Space: 48.9 MB (31.89%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -6,9 +6,9 @@ var rob = function(nums) {
     const dp=new Array(nums).fill(0)
     if(nums.length===1) return nums[0]
     dp[0]=nums[0]
-    dp[1]=nums[1]
+    dp[1]=Math.max(nums[0],nums[1])
     for(let i=2;i<nums.length;i++){
-        dp[i]=Math.max(...dp.slice(0,i-1))+nums[i]
+        dp[i]=Math.max(dp[i-1],nums[i]+dp[i-2])
     }
-    return Math.max(...dp)
+    return dp[dp.length-1]
 };
