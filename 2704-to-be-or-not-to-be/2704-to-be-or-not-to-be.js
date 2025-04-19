39faad0c91e8b1aed76481aad7c83c9f53793857
이효진 <oneNmore.jin@gmail.com>
2025-04-19 20:21:05 +0900

    Time: 35 ms (89.68%), Space: 53.2 MB (76.48%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe:(compare)=>{
+            if(compare===val){
+                return true
+            }else{
+                throw Error('Not Equal')
+            }
+        },
+        notToBe:(compare)=>{
+            if(compare!==val){
+                return true
+            }else{
+                throw Error('Equal')
+            }
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
