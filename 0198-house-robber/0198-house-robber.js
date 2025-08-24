c20452c1e952187b0f14e725dd057e81f138b841
이효진 <oneNmore.jin@gmail.com>
2025-08-24 18:00:51 +0900

    Time: 0 ms (100%), Space: 53.3 MB (70.17%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -3,12 +3,19 @@
  * @return {number}
  */
 var rob = function(nums) {
-    const dp=new Array(nums).fill(0)
     if(nums.length===1) return nums[0]
+    
+    const dp=Array(nums.length).fill(0)
+
+    // 1 3 3 1 1
+    // 3 1 1 3 1
+
     dp[0]=nums[0]
-    dp[1]=Math.max(nums[0],nums[1])
-    for(let i=2;i<nums.length;i++){
-        dp[i]=Math.max(dp[i-1],nums[i]+dp[i-2])
+    dp[1]=nums[1]
+
+    for(let i=2;i<dp.length;i++){
+        dp[i]=Math.max(...dp.slice(0,i-1))+nums[i]
     }
-    return dp[dp.length-1]
+    // console.log(dp)
+    return Math.max(...dp)
 };
