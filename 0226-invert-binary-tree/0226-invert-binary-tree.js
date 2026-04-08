fd010cca68c6aad7f78ba91e503ad5650cb489cb
이효진 <oneNmore.jin@gmail.com>
2026-04-08 21:55:23 +0900

    Time: 0 ms (100%), Space: 54.8 MB (11.43%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+        if(root===null) return null
+        const tmp=root.right
+        root.right=root.left
+        root.left=tmp
+        invertTree(root.left)
+        invertTree(root.right)
+        return root
+};
